98ded12549402333ced5ba8c238e29900eb3ff8c
Zvexhal <devmchtr@gmail.com>
2022-08-15 20:32:15 +0200

    feat: Part 5 done

--- part05-Part05_05.ConstructorOverload/src/main/java/Product.java
+++ part05-Part05_05.ConstructorOverload/src/main/java/Product.java
@@ -5,6 +5,19 @@ public class Product {
     private String location;
     private int weight;
 
+    public Product(String name){
+        this(name,"shelf",1);
+    }
+
+    public Product(String name,String location){
+        this(name,location,1);
+    }
+
+    public Product(String name,int weight){
+        this(name,"shelf",weight);
+    }
+
+
     public Product(String name, String location, int weight) {
         this.name = name;
         this.location = location;
