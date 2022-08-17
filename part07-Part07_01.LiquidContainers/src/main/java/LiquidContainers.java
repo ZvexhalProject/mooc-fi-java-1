ed5881351280483543b6e98f813847dd588b8bc8
Zvexhal <devmchtr@gmail.com>
2022-08-17 19:26:57 +0200

    feat: Part 7 until 3 done

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -1,19 +1,58 @@
-
 import java.util.Scanner;
 
 public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int amountContainer1=0;
+        int amountContainer2=0;
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: "+amountContainer1+"/100");
+            System.out.println("Second: "+amountContainer2+"/100");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
+            String [] inputSplit=input.split(" ");
+            if(inputSplit[0].equals("add")){
+                if(amountContainer1+Integer.valueOf(inputSplit[1])>100){
+                    amountContainer1=100;
+                    continue;
+                }
+                if(Integer.valueOf(inputSplit[1])<0){
+                    continue;
+                }
+                amountContainer1+=Integer.valueOf(inputSplit[1]);
+            }
+
+            if(inputSplit[0].equals("move")){
+                if(amountContainer1-Integer.valueOf(inputSplit[1])<0){
+                    amountContainer2+=amountContainer1;
+                    amountContainer1=0;
+                    continue;
+                }
+                amountContainer1-=Integer.valueOf(inputSplit[1]);
+                
+                if(amountContainer2+Integer.valueOf(inputSplit[1])>100){
+                    amountContainer2=100;
+                    continue;
+                }
+                amountContainer2+=Integer.valueOf(inputSplit[1]);
+            }
+
+            if(inputSplit[0].equals("remove")){
+                if(amountContainer2-Integer.valueOf(inputSplit[1])<0){
+                    amountContainer2=0;
+                    continue;
+                }
+                if(Integer.valueOf(inputSplit[1])<0){
+                    continue;
+                }
+                amountContainer2-=Integer.valueOf(inputSplit[1]);
+            }
+
 
         }
     }
--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -7,13 +7,38 @@ public class LiquidContainers2 {
         Scanner scan = new Scanner(System.in);
 
 
+        Container amountContainer1=new Container();
+        Container amountContainer2=new Container();
+
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: "+amountContainer1);
+            System.out.println("Second: "+amountContainer2);
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
+            String [] inputSplit=input.split(" ");
+            if(inputSplit[0].equals("add")){
+                amountContainer1.add(Integer.valueOf(inputSplit[1]));
+                continue;
+            }
+
+            if(inputSplit[0].equals("move")){
+                int tempAmountC1=amountContainer1.contains();
+                amountContainer1.remove(Integer.valueOf(inputSplit[1]));
+                if(amountContainer1.contains()==0){
+                    amountContainer2.add(tempAmountC1);
+                    continue;
+                }
+                amountContainer2.add(Integer.valueOf(inputSplit[1]));
+            }
+
+            if(inputSplit[0].equals("remove")){
+                amountContainer2.remove(Integer.valueOf(inputSplit[1]));
+                continue;
+            }
+
 
         }
     }
